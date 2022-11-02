43366dbd78445520c8e5abe363c3c7618d8fd507
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-11-02 19:33:20 +0100

    WIP

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from pkg.accounts import Accounts
-from pkg.transactions import Transactions
+import csv
+from pkg.transactions.transactions import Transactions
+from pkg.categories import get_category
 
-
-print("ACCOUNTS")
-res = Accounts().get()
-print(res.json())
 
 print("TRANSACTIONS")
-res = Transactions().get()
-print(res.json())
+transactions = Transactions().get()
+
+
+def get_real_amount(value):
+    return int(value.unscaled_value) / (10 * int(value.scale))
+
+
+with open('output.csv', 'w') as f:
+    writer = csv.writer(f,  delimiter=';')
+    for transaction in transactions.transactions:
+        category = get_category(transaction.descriptions.original)
+        if category is None:
+            print(transaction.descriptions.display)
+        writer.writerow((
+            transaction.dates.value,
+            0,
+            transaction.descriptions.display,
+            None,
+            "",  # memo
+            get_real_amount(transaction.amount.value),
+            category,
+            None
+        ))
