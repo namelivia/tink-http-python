21f4224f25ee12a999c918458d7dc1ed83495e02
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-11-18 12:54:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="tink-http-python",
-    version="0.0.11",
+    version="1.0.0",
     description="Python SDK for accessing the tink API ",
     long_description=long_description,
     long_description_content_type="text/markdown",
