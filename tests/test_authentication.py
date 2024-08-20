94e94ba58b4084eeb8517125101baa4f335eadaa
José Ignacio Amelivia Santiago <hello@namelivia.com>
2024-08-20 09:17:57 +0200

    Update authentication not to use refresh tokens

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -16,39 +16,18 @@ class TestAuthentication:
         )
         self.authentication = Authentication(self.api, self.storage, self.config)
 
-    def test_get_refresh_token(self):
-        self.storage.retrieve_authorization_code.return_value = "AUTHORIZATION_CODE"
-        self.api.post.return_value = {"refresh_token": "REFRESH_TOKEN"}
-        self.authentication.get_refresh_token()
-        self.api.post.assert_called_once_with(
-            "oauth/token",
-            {},
-            {
-                "client_id": "CLIENT_ID",
-                "code": "AUTHORIZATION_CODE",
-                "client_secret": "CLIENT_SECRET",
-                "grant_type": "authorization_code",
-            },
-        )
-        self.storage.store_new_refresh_token_refresh_token.assert_called_once_with(
-            "REFRESH_TOKEN"
-        )
-
     def test_get_access_token(self):
-        self.storage.retrieve_refresh_token.return_value = "REFRESH_TOKEN"
-        self.api.post.return_value = {
-            "access_token": "ACCESS_TOKEN",
-            "refresh_token": "REFRESH_TOKEN",
-        }
+        self.storage.retrieve_authorization_code.return_value = "AUTHORIZATION_CODE"
+        self.api.post.return_value = {"access_token": "ACCESS_TOKEN"}
         access_token = self.authentication.get_access_token()
         self.api.post.assert_called_once_with(
             "oauth/token",
             {},
             {
                 "client_id": "CLIENT_ID",
-                "refresh_token": "REFRESH_TOKEN",
                 "client_secret": "CLIENT_SECRET",
-                "grant_type": "refresh_token",
+                "code": "AUTHORIZATION_CODE",
+                "grant_type": "authorization_code",
             },
         )
         assert access_token == "ACCESS_TOKEN"
--- tink_http_python/authentication/authentication.py
+++ tink_http_python/authentication/authentication.py
@@ -20,27 +20,9 @@ class Authentication:
 
         return f"https://link.tink.com/1.0/transactions/connect-accounts/?client_id={self.config.client_id}&redirect_uri={link}&market=ES&locale=es_ES"
 
-    def get_refresh_token(self) -> str:
+    def get_access_token(self) -> str:
         code = self.storage.retrieve_authorization_code()
         token_response = self.api.get_new_access_token(
             self.config.client_id, self.config.client_secret, code
-        )
-        self.storage.store_new_refresh_token_refresh_token(
-            token_response["refresh_token"]
-        )
-        return token_response["refresh_token"]
-
-    def get_access_token(self) -> str:
-        try:
-            refresh_token = self.storage.retrieve_refresh_token()
-        except NoRefreshTokenException:
-            refresh_token = self.get_refresh_token()
-        token_response = self.api.refresh_token(
-            self.config.client_id,
-            self.config.client_secret,
-            refresh_token,
-        )
-        self.storage.store_new_refresh_token_refresh_token(
-            token_response["refresh_token"]
         )
         return token_response["access_token"]
