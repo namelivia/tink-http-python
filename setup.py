e7ee639dcd74870fd456c47315eabe5527563751
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-08-07 16:51:43 +0200

    Upgrade api types

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="tink-http-python",
-    version="1.3.2",
+    version="1.4.1",
     description="Python SDK for accessing the tink API ",
     long_description=long_description,
     long_description_content_type="text/markdown",
